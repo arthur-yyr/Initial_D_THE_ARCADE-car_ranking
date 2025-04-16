09ca1a53e5f773ef090d71fbec07f5c2976766b6
yayoru <shunhong1120120@gmail.com>
2025-04-17 03:53:42 +0900

    change update time to JST 0600

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ import main
 from datetime import datetime, timezone, timedelta
 
 if __name__ == "__main__":
-    schedule.every().day.at("07:00").do(main.updatedata, sleeptime = 20)
+    schedule.every().day.at("06:00").do(main.updatedata, sleeptime = 20)
     log_time = datetime.now(timezone(timedelta(hours=9))).isoformat(timespec='microseconds')
     with open("scheduler.log", "w+") as f:
         f.seek(0)
